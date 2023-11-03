401741f79ca8ef90c8506506c6791362ca1fbe79
MajuEspinoza <julia.espinozarate@gmail.com>
2023-11-02 22:09:36 -0300

    Subiendo archivo para lectura de ADN mutante o no mutante

---
+++ parcial2.py
@@ -0,0 +1,46 @@
+def is_mutant(dna):
+    def check_sequence(sequence):
+        return (sequence.count("AAAA") or sequence.count("aaaa")) >= 1 or (sequence.count("TTTT") or sequence.count("tttt")) >= 1 or (sequence.count("CCCC") or sequence.count("cccc")) >= 1 or (sequence.count("GGGG") or sequence.count("gggg"))>= 1
+
+    # Verificar filas
+    for row in dna:
+        if check_sequence(row):
+            return True
+
+    # Verificar columnas
+    for col in range(len(dna[0])):
+        column_sequence = "".join(dna[row][col] for row in range(len(dna)))
+        if check_sequence(column_sequence):
+            return True
+
+    # Verificar diagonales principales
+    for i in range(len(dna) - 3):
+        for j in range(len(dna[0]) - 3):
+            diagonal_sequence = "".join(dna[i + k][j + k] for k in range(4))
+            if check_sequence(diagonal_sequence):
+                return True
+
+    # Verificar diagonales secundarias
+    for i in range(len(dna) - 3):
+        for j in range(len(dna[0]) - 3):
+            diagonal_sequence = "".join(dna[i + k][j + 3 - k] for k in range(4))
+            if check_sequence(diagonal_sequence):
+                return True
+
+    return False
+
+def main():
+    # Solicitar al usuario las secuencias de ADN y almacenarlas en una lista
+    dna = []
+    for i in range(6):
+        row = input(f"Ingrese la secuencia de ADN para la fila {i + 1}: ").upper()
+        dna.append(row)
+
+    # Verificar si es mutante
+    if is_mutant(dna):
+        print("El ADN corresponde a un mutante.")
+    else:
+        print("El ADN no corresponde a un mutante.")
+
+if __name__ == "__main__":
+    main()
