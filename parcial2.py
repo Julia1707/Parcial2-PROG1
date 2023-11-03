2815b1760f0797f4659370331bbef91509f1b09d
MajuEspinoza <julia.espinozarate@gmail.com>
2023-11-02 22:44:25 -0300

    Modificaciones para que tome repeticiones en las columnas y limitacion en lo que puede ingresar al cargar el adn

--- parcial2.py
+++ parcial2.py
@@ -1,6 +1,6 @@
 def is_mutant(dna):
     def check_sequence(sequence):
-        return (sequence.count("AAAA") or sequence.count("aaaa")) >= 1 or (sequence.count("TTTT") or sequence.count("tttt")) >= 1 or (sequence.count("CCCC") or sequence.count("cccc")) >= 1 or (sequence.count("GGGG") or sequence.count("gggg"))>= 1
+        return (sequence.count("AAAA") or sequence.count("aaaa"))>= 1 or (sequence.count("TTTT")or sequence.count("tttt") )>= 1 or (sequence.count("CCCC") or sequence.count("cccc")) >= 1 or (sequence.count("GGGG")or sequence.count("gggg")) >= 1
 
     # Verificar filas
     for row in dna:
@@ -33,14 +33,20 @@ def main():
     # Solicitar al usuario las secuencias de ADN y almacenarlas en una lista
     dna = []
     for i in range(6):
-        row = input(f"Ingrese la secuencia de ADN para la fila {i + 1}: ").upper()
-        dna.append(row)
+        valid_input = False
+        while not valid_input:
+            row = input(f"Ingrese la secuencia de ADN para la fila {i + 1}: ").upper()
+            if all(base in "ATCG" for base in row):
+                valid_input = True
+                dna.append(row)
+            else:
+                print("Error: La secuencia de ADN solo puede contener las letras A, T, C y G. Inténtelo de nuevo.")
 
     # Verificar si es mutante
     if is_mutant(dna):
-        print("El ADN corresponde a un mutante.")
+        print("El ADN ingresado corresponde a un mutante.")
     else:
-        print("El ADN no corresponde a un mutante.")
+        print("El ADN ingresado no corresponde a un mutante.")
 
 if __name__ == "__main__":
     main()
